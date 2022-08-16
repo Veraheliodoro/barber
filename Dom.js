61e3c4561b4156a778b59e99664b1e342ae1f401
Veraheliodoro <veraheliodoro@gmail.com>
2022-08-16 15:38:05 +0200

    exercice java

--- Dom.js
+++ Dom.js
@@ -10,10 +10,14 @@ div.classList.add("newdiv"); // adicioner une classe au div
 body.appendChild(div); // adicioner le Div au document ( body)
 
 // cree et adicioner un div au body avec Class
-let condition = true ; 
+let condition = false ; 
 let M = document.createElement("div"); // creer un element
 let Y = document.querySelector("body"); // selectioner body
-if (condition==true){
+if (!condition){ // boolear podes utilisar ou nao == OU ===
     Y.appendChild(M); // adicioner le Div au document ( body)
 }
 M.setAttribute('id','new1div'); // adicioner une classe au div
+
+ function myfuction(a , b) { // js e morto nao chama funcion et chamado
+    console.log(a * b)
+}
